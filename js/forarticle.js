9996ac584e8aa3d72756c333ef3917d44bb84234
Vitaliy-1 <vitaliybezsh@gmail.com>
2016-11-24 21:38:15 +0200

    Initial commit

--- js/forarticle.js
+++ js/forarticle.js
@@ -49,6 +49,7 @@ $("a[href='#figuresdata']").click(function() {
 
 /* Reference pop-ups */
 
+/*
 
 $(function () {
   $('[class="ref-tip"]').tooltip()
@@ -59,16 +60,50 @@ var bibArr = jQuery.makeArray(document.getElementsByClassName("bib"));
 
 for (var i = 0; i < bibArr.length; i++) {
 	bibArr[i] = bibArr[i].innerText;
-    var p = i + 1;
-	$("[rid='bib" + p + "']").attr("title", bibArr[i]);
+      var p = i + 1;
+  	$("[rid='bib" + p + "']").attr("title", bibArr[i]);
+}
+
+*/
+
+$(function () {
+  $('[class="ref-tip btn btn-default"]').popover()
+});
+
+/*
+$("button.ref-tip.btn.btn-default").attr("data-placement", "top");
+
+var myText = $("span.bib#bib1").html();
+
+$(myText).appendTo("body");
+*/
+$('[class="ref-tip btn btn-default"]').attr("data-placement", "top")
+
+var refAuth = jQuery.makeArray(document.getElementsByClassName("ref-auth"));
+var refTitle = jQuery.makeArray(document.getElementsByClassName("ref-title"));
+var refSource = jQuery.makeArray(document.getElementsByClassName("ref-source"));
+var refInfo = jQuery.makeArray(document.getElementsByClassName("ref-info"));
+var refFullText = jQuery.makeArray(document.getElementsByClassName("ref-full"));
+
+for (var i = 0; i < refAuth.length; i++) {
+  refAuth[i] = refAuth[i].innerHTML;
+  refTitle[i] = refTitle[i].innerHTML;
+  refSource[i] = refSource[i].innerHTML;
+  refInfo[i] = refInfo[i].innerHTML;
+  refFullText[i] = refFullText[i].interHTML;
+};
+  
+for (var i = 0; i < refAuth.length; i++) {
+    output = [refAuth[i] + refSource[i] + refInfo[i]];
+    for (var s = 0; s < output.length; s++) {
+     var p = i + 1;
+    $("[rid='bib" + p + "']").attr("data-content", output);
+    }
+};
+ 
+for (var i = 0; i < refTitle.length; i++) {
+      var p = i + 1;
+    $("[rid='bib" + p + "']").attr("title", refTitle[i]);
 }
 
 
-
-/*
-bibArr[0] = bibArr[0].innerText;
-$("[reference=1]")[0].setAttribute("title", bibArr[0]);
-
-bibArr[1] = bibArr[1].innerText;
-$("[reference=2]")[0].setAttribute("title", bibArr[1]);
-*/
