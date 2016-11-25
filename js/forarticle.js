63f612deb9cfb2723594259cfbaac3fed5858057
Vitaliy-1 <vitaliybezsh@gmail.com>
2016-11-25 14:46:56 +0200

    Initial commit

--- js/forarticle.js
+++ js/forarticle.js
@@ -32,7 +32,7 @@ $('#myTabs li:eq(1) a').click(function (e) {
 
 /* Clone figures for tabs */
 
-$("div.row.tab-content").append("<div class='col-lg-8 col-md-8 col-sm-12 col-xs-12 tab-pane fade' id='figuresdata'></div>")
+$("div.row.tab-content").append("<div class='col-lg-7 col-md-7 col-sm-12 col-xs-12 tab-pane fade' id='figuresdata'></div>")
 
 $(document).ready(function(){
     $("div.figure-wrap").each(function(){
@@ -64,9 +64,6 @@ $('[class="ref-tip btn btn-info"]').attr("tabindex", "0");
 $('[class="ref-tip btn btn-info"]').attr("role", "button");
 $('[class="ref-tip btn btn-info"]').attr("data-trigger", "hover");
 
-
-
-
 var refAuth = jQuery.makeArray(document.getElementsByClassName("ref-auth"));
 var refTitle = jQuery.makeArray(document.getElementsByClassName("ref-title"));
 var refSource = jQuery.makeArray(document.getElementsByClassName("ref-source"));
@@ -77,12 +74,11 @@ for (var i = 0; i < refAuth.length; i++) {
   refAuth[i] = refAuth[i].innerHTML;
   refTitle[i] = refTitle[i].innerHTML;
   refSource[i] = refSource[i].innerHTML;
-  refInfo[i] = refInfo[i].innerHTML;
   refFullText[i] = refFullText[i].innerHTML;
 };
   
 for (var i = 0; i < refAuth.length; i++) {
-    output = [refAuth[i] + ' ' + refSource[i] + ' ' + refInfo[i]];
+    output = [refAuth[i] + ' ' + refSource[i]];
     for (var s = 0; s < output.length; s++) {
      var p = i + 1;
     $("[rid='bib" + p + "']").attr("data-content", output);
@@ -95,3 +91,6 @@ for (var i = 0; i < refTitle.length; i++) {
 };
 
 
+/* Tables */
+
+$('table').attr("class", "table table-striped table-bordered");
