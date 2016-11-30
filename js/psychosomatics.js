c0a40771dc87f87d908699ebd5dff241dbac3a46
Vitaliy-1 <vitaliybezsh@gmail.com>
2016-12-01 01:04:40 +0200

    Initial commit

--- js/psychosomatics.js
+++ js/psychosomatics.js
@@ -80,11 +80,11 @@ var refAuth = jQuery.makeArray(document.getElementsByClassName("ref-auth"));
 var refTitle = jQuery.makeArray(document.getElementsByClassName("ref-title"));
 var refSource = jQuery.makeArray(document.getElementsByClassName("ref-source"));
 var refInfo = jQuery.makeArray(document.getElementsByClassName("ref-info"));
-var refFullText = jQuery.makeArray(document.getElementsByClassName("ref-full"));
+//var refFullText = jQuery.makeArray(document.getElementsByClassName("ref-full"));
 
 for (var i = 0; i < refAuth.length; i++) {
   refTitle[i] = refTitle[i].innerHTML;
-  refFullText[i] = refFullText[i].innerHTML;
+//  refFullText[i] = refFullText[i].innerHTML;
 };
   
 for (var i = 0; i < refAuth.length; i++) {
@@ -98,16 +98,6 @@ for (var i = 0; i < refTitle.length; i++) {
       var p = i + 1;
     $("[rid='bib" + p + "']").attr("title", refTitle[i]);
 };
-
-/* Author popover */
-$("[ref='aff1'").hover(function(){
-        $("[iid='aff1']").show();
-      },
-      function() {
-        $("[iid='aff1']").hide();
-      
-    });
-
 
 /* Tables */
 
@@ -197,73 +187,54 @@ $(window).resize(function() {
     }
 })(jQuery);
 
-
-
-/* There is a need to write a cycle */
-
 var collapseNumber = jQuery.makeArray(document.getElementsByClassName("panel panel-default"));
 for (let i = 0; i < collapseNumber.length; i++) {
   $('#collapse' + i).on('shown.bs.collapse', function () {
      $('#s' + i).goTo();  
    });
-}
+};
 
+ /* Authors Affilaation */
 
-/*
-$('#collapse1').on('shown.bs.collapse', function () {
-     $('#s1').goTo();  
+jQuery.fn.getEvents = function() {
+    if (typeof(jQuery._data) == 'function') {
+        return jQuery._data(this.get(0), 'events') || {};
+    } else if (typeof(this.data) == 'function') { 
+        return this.data('events') || {};
+    }
+    return {};
+};
+
+jQuery.fn.preBind = function(type, data, fn) {
+    this.each(function () {
+        var $this = jQuery(this);
+
+        $this.bind(type, data, fn);
+
+        var currentBindings = $this.getEvents()[type];
+        if (jQuery.isArray(currentBindings)) {
+            currentBindings.unshift(currentBindings.pop());
+        }
+    });
+    return this;
+};
+
+for (let i = 0; i < 10; i++) {
+  $("[data-aff *=" + i + "]").bind('click', function() {
+    if ($( "[data-id =" + i + "]").is(':hidden')) {
+      $( "[data-id =" + i + "]").show( "slow")
+    } else {$( "[data-id =" + i + "]").hide("slow")}
+  });
+};
+
+$("[data-aff]").preBind('click', function() {
+  $("[data-id]").hide();
 });
 
-$('#collapse2').on('shown.bs.collapse', function () {
-     $('#s2').goTo();  
-});
-
-$('#collapse3').on('shown.bs.collapse', function () {
-     $('#s3').goTo();  
-});
-
-$('#collapse4').on('shown.bs.collapse', function () {
-     $('#s4').goTo();  
-});
-
-$('#collapse5').on('shown.bs.collapse', function () {
-     $('#s5').goTo();  
-});
-
-$('#collapse6').on('shown.bs.collapse', function () {
-     $('#s6').goTo();  
-});
-
-$('#collapse7').on('shown.bs.collapse', function () {
-     $('#s7').goTo();  
-});
-
-$('#collapse8').on('shown.bs.collapse', function () {
-     $('#s8').goTo();  
-});
-
-$('#collapse9').on('shown.bs.collapse', function () {
-     $('#s9').goTo();  
-});
-
-$('#collapse10').on('shown.bs.collapse', function () {
-     $('#s10').goTo();  
-});
-
-$('#collapse11').on('shown.bs.collapse', function () {
-     $('#s11').goTo();  
-});
-
-$('#collapse12').on('shown.bs.collapse', function () {
-     $('#s12').goTo();  
-});
-*/
-/*Scroll-spy */
 
 
 
-$('[data-spy="scroll"]').each(function () {
-  var $spy = $(this).scrollspy('refresh')
-})
 
-$('body').scrollspy({ target: 'myAffix' })
+
+
+
